a1f86e875d594dfbc4818e7621c5cc8beb2b8ce2
Snedashkovsky <snedashkovsky@gmail.com>
2020-04-30 02:18:02 +0700

    Fix development mode bash

--- start_bot_dev.sh
+++ start_bot_dev.sh
@@ -1,4 +1,4 @@
-export VALIDATOR_QUERY=cat\ .\/cyberdBot\/test\/validators_query_test
+export VALIDATOR_QUERY=cat\ .\/tests\/validators_query_test
 export DEV_MODE=1
 export TELEBOT_TOKEN=<your bot token>
 python3 main.py
